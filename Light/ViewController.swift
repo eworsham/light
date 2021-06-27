01cdb27dc1d10254877d43b709d6636aed6fdda7
Ryan Worsham <eworsham64@gmail.com>
2021-06-26 18:15:19 -0600

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Ryan Worsham on 6/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
